5913bebedae2ffcf7ff602fb1e001416c0ed04ea
Jordan Huang <good5dog5@gmail.com>
2017-11-02 09:39:27 +0800

    Update docekr_cmd.sh.

--- linuxkit_moby_qcuda/docker_cmd.sh
+++ linuxkit_moby_qcuda/docker_cmd.sh
@@ -5,6 +5,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Ref https://stackoverflow.com/questions/33013539/docker-loading-kernel-modules
+
+# 起因於發現build qcu-driver時會找不到/lib/moudles/linux-xxx-headers，原本的想法是裝linux-headers
+# 但用掛載host dir to container的方法更優雅的解決這個問題
+
 docker run --privileged -p 3055:22 --cap-add=ALL \
     -v /dev:/dev \
     -v /lib/modules:/lib/modules \
